e26711447191bb0075f9edd69221c2af6cb004e0
psraynes <72121524+psraynes@users.noreply.github.com>
2025-03-03 23:50:52 -0500

    Load files

--- kondo_cube.py
+++ kondo_cube.py
@@ -22,8 +22,8 @@ def main():
     pil_image.save(png_bio, format='PNG')
 
     # Initialize Window
-    layout = [[sg.Column([[sg.Text("Sparks Joy:", font='bold')],
-                          [sg.Listbox(values=yes_list, expand_x=True, expand_y=True, key='-YES_LIST-')],
+    layout = [[sg.Column([[sg.Text("Sparks Joy:", font='bold'), sg.Text('0', key='-YES_COUNT-')],
+                          [sg.Listbox(values=yes_list, expand_x=True, expand_y=True, horizontal_scroll=True, size=25, key='-YES_LIST-')],
                           [sg.Button('Save Kondo Cube')]],
                           vertical_alignment='top', expand_y=True),
               sg.Column([[sg.Button('Load Kondo Cube'), sg.Push(), sg.Button('Load Anti-Kondo Cube')],
@@ -32,8 +32,8 @@ def main():
                          [sg.Button('Yes', size=10), sg.Button('No', size=10)]],
                          vertical_alignment='top',
                          element_justification='center'),
-              sg.Column([[sg.Text("Does Not Spark Joy:", font='bold')],
-                         [sg.Listbox(values=no_list, expand_x=True, expand_y=True, key='-NO_LIST-')],
+              sg.Column([[sg.Text("Does Not Spark Joy:", font='bold'), sg.Text('0', key='-NO_COUNT-')],
+                         [sg.Listbox(values=no_list, expand_x=True, expand_y=True, horizontal_scroll=True, size=25, key='-NO_LIST-')],
                          [sg.Button('Save Anti-Kondo Cube')]],
                          vertical_alignment='top', expand_y=True)]]
 
@@ -69,23 +69,40 @@ def main():
             # Update image
             window['-IMAGE-'].update(data=png_bio.getvalue())
 
+            # Update counts
+            window['-YES_COUNT-'].update(len(yes_list))
+            window['-NO_COUNT-'].update(len(no_list))
+
+        # Save and load files. Lists are stored as individual names per line
         if event == 'Load Kondo Cube':
-            print('load event')
+            filename = sg.popup_get_file('Load Kondo Cube', no_window=True)
+            if filename:
+                with open(filename, 'r') as file:
+                    yes_list = file.readlines()
+                    window['-YES_LIST-'].update(yes_list)
+                    window['-YES_COUNT-'].update(len(yes_list))
 
         if event == 'Load Anti-Kondo Cube':
-            print('anti load event')
+            filename = sg.popup_get_file('Load Anti-Kondo Cube', no_window=True)
+            if filename:
+                with open(filename, 'r') as file:
+                    no_list = file.readlines()
+                    window['-NO_LIST-'].update(no_list)
+                    window['-NO_COUNT-'].update(len(no_list))
 
         if event == 'Save Kondo Cube':
-            filename = sg.popup_get_file('Save As', save_as=True, no_window=True, default_path=r'kondo-cube.txt')
-            with open(filename, 'w', newline='') as file:
-                for card_name in yes_list:
-                    file.write(card_name + '\n')
+            filename = sg.popup_get_file('Save Kondo Cube', save_as=True, no_window=True, default_path=r'kondo-cube.txt')
+            if filename:
+                with open(filename, 'w', newline='') as file:
+                    for card_name in yes_list:
+                        file.write(card_name + '\n')
 
         if event == 'Save Anti-Kondo Cube':
-            filename = sg.popup_get_file('Save As', save_as=True, no_window=True, default_path=r'anti-kondo-cube.txt')
-            with open(filename, 'w', newline='') as file:
-                for card_name in no_list:
-                    file.write(card_name + '\n')
+            filename = sg.popup_get_file('Save Anti-Kondo Cube', save_as=True, no_window=True, default_path=r'anti-kondo-cube.txt')
+            if filename:
+                with open(filename, 'w', newline='') as file:
+                    for card_name in no_list:
+                        file.write(card_name + '\n')
 
 if __name__ == '__main__':
     main()
