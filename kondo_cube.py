bb21b2ac4a2afc5f9d0d61277164fe3d6aeea7df
psraynes <72121524+psraynes@users.noreply.github.com>
2025-03-03 23:21:45 -0500

    save lists

--- kondo_cube.py
+++ kondo_cube.py
@@ -3,6 +3,7 @@ import scrython as scry
 import FreeSimpleGUI as sg
 from PIL import Image
 import requests as req
+import csv
 
 
 def main():
@@ -23,14 +24,17 @@ def main():
     # Initialize Window
     layout = [[sg.Column([[sg.Text("Sparks Joy:", font='bold')],
                           [sg.Listbox(values=yes_list, expand_x=True, expand_y=True, key='-YES_LIST-')],
-                          [sg.Button('Yes')]],
+                          [sg.Button('Save Kondo Cube')]],
                           vertical_alignment='top', expand_y=True),
-              sg.Column([[sg.Text("Does this spark joy?")], 
-                         [sg.Image(data=png_bio.getvalue(), key = '-IMAGE-')]], 
-                         vertical_alignment='top'),
+              sg.Column([[sg.Button('Load Kondo Cube'), sg.Push(), sg.Button('Load Anti-Kondo Cube')],
+                         [sg.Text("Does this spark joy?")],
+                         [sg.Image(data=png_bio.getvalue(), key = '-IMAGE-')],
+                         [sg.Button('Yes', size=10), sg.Button('No', size=10)]],
+                         vertical_alignment='top',
+                         element_justification='center'),
               sg.Column([[sg.Text("Does Not Spark Joy:", font='bold')],
                          [sg.Listbox(values=no_list, expand_x=True, expand_y=True, key='-NO_LIST-')],
-                         [sg.Button('No')]],
+                         [sg.Button('Save Anti-Kondo Cube')]],
                          vertical_alignment='top', expand_y=True)]]
 
     window = sg.Window('Kondo Cube Generator', layout)
@@ -39,9 +43,9 @@ def main():
     while True:
 
         event, values = window.read()
-
         if event == sg.WINDOW_CLOSED:
             break
+
         if event == 'Yes' or event == 'No':
             if event == 'Yes':
                 # Add to Yes List
@@ -64,6 +68,24 @@ def main():
 
             # Update image
             window['-IMAGE-'].update(data=png_bio.getvalue())
+
+        if event == 'Load Kondo Cube':
+            print('load event')
+
+        if event == 'Load Anti-Kondo Cube':
+            print('anti load event')
+
+        if event == 'Save Kondo Cube':
+            filename = sg.popup_get_file('Save As', save_as=True, no_window=True, default_path=r'kondo-cube.txt')
+            with open(filename, 'w', newline='') as file:
+                for card_name in yes_list:
+                    file.write(card_name + '\n')
+
+        if event == 'Save Anti-Kondo Cube':
+            filename = sg.popup_get_file('Save As', save_as=True, no_window=True, default_path=r'anti-kondo-cube.txt')
+            with open(filename, 'w', newline='') as file:
+                for card_name in no_list:
+                    file.write(card_name + '\n')
 
 if __name__ == '__main__':
     main()
